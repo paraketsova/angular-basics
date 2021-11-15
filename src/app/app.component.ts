69ffb49b4b9c3f5500cb21cdfc4da58634113d7a
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-15 19:05:30 +0100

    Delete exercises - clean project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,34 +8,5 @@ import {timeout} from "rxjs";
 })
 export class AppComponent { //здесь заводим все динамические переменные
 
-  title = 'Dynamic title'
-  number = 42
-  arr = [1, 2, 3]
 
-  obj = { a:1, b: {c: 2} }
-
-  // img = 'https://miro.medium.com/max/400/1*PWe4DmAE78BLD4SHpXizMw.png'
-
-  inputValue = ''
-
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is over')
-    //   this.img = 'https://angular.io/assets/images/logos/angular/angular.png'
-    // }, 5000)
-  }
-
-//  создаем метод, который будем вызывать в HTML
-  onInput(event: any) {
-    // console.log('Event', event);
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-
-  onBlur(str: string) {
-  this.inputValue = str
-  }
-
-  onClick() {
-    console.log('Click!')
-  }
 }
