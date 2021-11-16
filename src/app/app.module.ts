32bce5038f25e6fa561200623a7ad27b87120f23
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-16 14:08:15 +0100

    Add empty components post and post-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {PostFormComponent} from './post-form/post-form.component';
+import {PostComponent} from './post/post.component';
 
 @NgModule({ // декоратор
   declarations: [
     AppComponent,
+    PostFormComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
