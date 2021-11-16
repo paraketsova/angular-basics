cd53962fca0bba98a9b14586152766b66a6386c3
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-16 15:25:44 +0100

    Add optional myPost to post components structure

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -8,8 +8,8 @@ import {Post} from "../app.component";
 })
 export class PostComponent implements OnInit {
 
-  @Input() post!: Post
-
+  @Input('myPost') post!: Post //передали в строке имя ожидаемого параметра, который может отличаться от шаблонного имени (post)
+//однако значения из перменной майПост будут заноситься в шаблон Пост
   constructor() { }
 
   ngOnInit(): void {
