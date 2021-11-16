51d1676e3bb23286da8e23c7fe0b00203b115748
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-16 15:42:45 +0100

    Add new post functional - write at console log

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Post} from "../app.component";
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +8,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
+  title = ''
+  text = ''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addPost(): void {
+    if (this.text.trim() && this.title.trim()) {
+      const post: Post = {
+        title: this.title,
+        text: this.text
+      }
+      console.log('New Post: ', post)
+      this.title = this.text = ''
+    }
   }
 
 }
