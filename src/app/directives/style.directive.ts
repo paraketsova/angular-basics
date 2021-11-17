b761117d89fa457fa20d1410461b2de25bef5232
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-17 20:21:11 +0100

    Directives - get parameters to a directive - +1

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -7,7 +7,7 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/co
 export class StyleDirective {
 
   @Input('appStyle') color: string = 'blue'
-  @Input('fontWeight') fontWeight = 'normal'
+  @Input() 'dStyles': {border?: string, fontWeight?: string, borderRadius?: string}
 
   constructor(private el: ElementRef, private r: Renderer2) {
     // console.log(el)
@@ -27,7 +27,9 @@ export class StyleDirective {
   @HostListener('mouseenter') onEnter() {
     this.r.setStyle(this.el.nativeElement, 'color', this.color)
     //при наведении мыши на параграф - добавляем новый цвет параграфу(он становится синим).
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyles.borderRadius)
 
 }
 
@@ -35,6 +37,8 @@ export class StyleDirective {
     this.r.setStyle(this.el.nativeElement, 'color', null)
     // ... При уходе мыши цвет с параграфа исчезает
     this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
+    this.r.setStyle(this.el.nativeElement, 'border', null)
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', null)
 
   }
 
