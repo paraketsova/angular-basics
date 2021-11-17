9c7ec3e4a517b915b7c9d1bfbc18a936035681dd
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-17 18:51:17 +0100

    Directives - take the element

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,9 +1,18 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 
 export class StyleDirective {
+  constructor(private el: ElementRef, private r: Renderer2) {
+    // console.log(el)
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue') // этот вариант изменения стилей рабочий и корректный.
+      // он работает благодаря добавлению в конструктор приват р: Рендерер2
+/*
+    el.nativeElement.style.color = 'red' // - это не слишком удачный способ изменить стиль
+*/
+
+  }
 
 }
