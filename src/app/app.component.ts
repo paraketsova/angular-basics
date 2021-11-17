e7cb5b34c6bb02f632f3dea10b4930cafc460158
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-17 17:31:49 +0100

    Create directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 export interface Post {
   title: string
@@ -11,31 +11,6 @@ export interface Post {
   templateUrl: './app.component.html', // конфигурация для шаблона выбранного выше компонента
   styleUrls: ['./app.component.scss']  // конфиграция для стиля выбранного выше компонента
 })
-export class AppComponent implements OnInit { //здесь заводим все динамические переменные
-  posts: Post[] = [
-    {title: 'I want to survive', text: 'I want to believe', id: 1},
-    {title: 'I want to break free', text: 'I want to break free from your lies', id: 2}
-  ]
+export class AppComponent { //здесь заводим все динамические переменные
 
-  ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log('timeOut')
-    //   this.posts[0] = {
-    //     title: 'Changed!',
-    //     text: 'Text also changed!',
-    //     id: 33
-    //   }
-    // }, 5000)
-    //в апп компоненте спустя неск секунд меняется элемент и энгуляр понял что изменилось и нарисовал в шаблоне
-  }
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post)
-    // console.log('Post', post)
-  }
-
-  removePost(id: number) {
-    console.log('I want to remove', id)
-    this.posts = this.posts.filter(p => p.id !==id)
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import {PostFormComponent} from './post-form/post-form.component';
-import {PostComponent} from './post/post.component';
+import {StyleDirective} from "./directives/style.directive";
+
 
 @NgModule({ // декоратор
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent,
+    StyleDirective
   ],
   imports: [
     BrowserModule,
