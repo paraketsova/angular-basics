df12b26c671d79d1c012c42cff5a0cb9b1ba9753
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-17 19:41:19 +0100

    Directives - add events

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
@@ -7,12 +7,30 @@ import {Directive, ElementRef, Renderer2} from "@angular/core";
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
     // console.log(el)
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue') // этот вариант изменения стилей рабочий и корректный.
-      // он работает благодаря добавлению в конструктор приват р: Рендерер2
+    // this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+    // этот вариант изменения стилей рабочий и корректный.
+    // он работает благодаря добавлению в конструктор приват р: Рендерер2
 /*
     el.nativeElement.style.color = 'red' // - это не слишком удачный способ изменить стиль
 */
-
   }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event)
+      // при клике на цветной абзац получаем в консоли объект евента или евент.таргет
+  }
+
+  @HostListener('mouseenter') onEnter() {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+    //при наведении мыши на параграф - добавляем новый цвет параграфу(он становится синим).
+
+}
+
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null)
+
+    // ... При уходе мыши цвет с параграфа исчезает
+  }
+
 
 }
