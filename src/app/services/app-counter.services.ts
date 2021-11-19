fa462a066b6f88fabd9c417355454dfb39903c97
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-19 11:56:31 +0100

    Services: decorator Injectable

--- src/app/services/app-counter.services.ts
+++ src/app/services/app-counter.services.ts
@@ -1,3 +1,13 @@
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+  //   а это ^ позволяет не добавлять наш AppCounterServices ручками
+  // в providers:[]  у @NgModule в app.module.ts
+})
+// этот декоратор Injectable рекомендуется ВСЕГДА добавлять к любому сервису.
+// он позволяет инжекрировать внутрь одного сервиса другой
+
 export class AppCounterServices {
   counter = 0
 
