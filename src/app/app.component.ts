c3a9477dfe038ea2fe79618056c941d4dc837968
Mariia Paraketsova <paraketsova@gmail.com>
2021-11-19 10:32:22 +0100

    Pipes - filtration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,24 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  [field: string]: string
+}
+
 @Component({
   selector: 'app-root', // именно этот селектор будет опознан для выбора и перенесён в HTML модуль при загрузке рендеринга
   templateUrl: './app.component.html', // конфигурация для шаблона выбранного выше компонента
   styleUrls: ['./app.component.scss']  // конфиграция для стиля выбранного выше компонента
 })
 export class AppComponent { //здесь заводим все динамические переменные
-  e: number = Math.E
 
-  str: string = 'hello world'
+  search = ''
+  searchField = 'title'
 
-  date: Date = new Date()
-
-  float = 0.42
-
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4
-      }
-    }
-  }
+ posts: Post[] = [
+   {title: 'One', text: "I want to break free!"},
+   {title: 'Two', text: "I've fallen in love..."},
+   {title: 'Three', text: "It's strange but it's true!"},
+ ]
 }
